6220601a0b8f19861329b0e6f1054344d40af760
SergeyMakeev <sergeymakeev@hotmail.com>
2025-08-03 00:29:46 -0700

    add bench cmd

--- analyze_benchmarks.cmd
+++ analyze_benchmarks.cmd
@@ -1,1 +1,2 @@
-python tools\analyze_benchmarks.py examples\performance_results.json --output tools\test_report.txt
+.\build2022\Release\performance_test.exe --benchmark_format=json --benchmark_out=performance_results.json
+python tools\analyze_benchmarks.py performance_results.json --output tools\test_report.txt
